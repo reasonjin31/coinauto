3c3fc5c954c20044f1c55425652fb30d9387a4f4
reasonjin31 <reasonjin31@naver.com>
2021-08-01 15:31:48 +0900

    3

--- test.py
+++ test.py
@@ -48,8 +48,9 @@ json_data = (json.loads(response.text))
 print(type(json_data))
 print(len(json_data))
 
-for i in len(json_data):
-    print(json_data[i]['market'])
+for i in json_data:
+    print("X")
+    # print(json_data[i]['market'])
  
 
 ####업비트 내 특정 코인의 24시간 거래대금 가져오기 
