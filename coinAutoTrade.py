92d6f90cf4a889644dd8d447d9fee85cafd0a5ef
reasonjin31 <reasonjin31@naver.com>
2021-08-07 19:51:24 +0900

    d

--- coinAutoTrade.py
+++ coinAutoTrade.py
@@ -269,13 +269,13 @@ while True:
         # 9:00 < 현재 < 8:59:50 사이에 타겟가를 충족 시 매수
         if start_time < now < sell_time :
 
-            if len(symbol_list) < 10:
-                time.sleep(1)
-                # 거래대금 상위 10 코인리스트(코인명,거래대금) 에서 코인명만 list에 넣기
-                df_sort_group_top10 = df_sort_group_top10()
-                symbol_list = df_sort_group_top10['coin'] #매수할 종목 리스트
-                print("Program Start! here is Symbolist")
-                print(symbol_list)
+            # if len(symbol_list) < 10:
+            time.sleep(1)
+            # 거래대금 상위 10 코인리스트(코인명,거래대금) 에서 코인명만 list에 넣기
+            df_sort_group_top10 = df_sort_group_top10()
+            symbol_list = df_sort_group_top10['coin'] #매수할 종목 리스트
+            print("Got Top10 Coin! here is Symbolist")
+            print(symbol_list)
 
             for sym in symbol_list:
                 # print("symbol "+ str(sym))
