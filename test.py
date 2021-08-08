f2806bdfb200110741160b47a0293af5838e8390
reasonjin31 <reasonjin31@naver.com>
2021-08-08 12:59:36 +0900

    d

--- test.py
+++ test.py
@@ -138,7 +138,7 @@ while True:
             print("i" , str(i))
             
             # if(str(balances[i]['currency']) != "KRW"):
-            tickers_temp = str(balances[i]['currency']),"-",str(balances[i]['unit_currency'])           
+            tickers_temp = balances[i]['currency'],"-",balances[i]['unit_currency']           
             print("tickers_temp : ", tickers_temp)
             currnet_price = pyupbit.get_orderbook(tickers=tickers_temp)[0]["orderbook_units"][0]["ask_price"]#현재가조회
             print("currnet_price :",currnet_price)
